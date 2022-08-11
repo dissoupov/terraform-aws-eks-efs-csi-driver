ec3ae5b67ce0b48732d95a35bd3141b2dcf17cc7
Denis Issoupov <denis@ekspand.com>
2022-08-11 18:58:53 +0100

    providers

--- versions.tf
+++ versions.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     aws        = ">= 4.0.0"
-    helm       = ">= 3.0"
-    kubernetes = ">= 3.0.0"
+    helm       = ">= 2.6.0"
+    kubernetes = ">= 2.12.1"
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.9.4"
